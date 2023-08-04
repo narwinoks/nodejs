7dbd375646e8411cdc0c5c2a122c0c459a3c0713
narwinoks <narnowin195@gmail.com>
2023-08-04 15:26:03 +0700

    Add file vercel.com

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ process.on("uncaughtException", (error) => {
 });
 
 // create server
-const server = app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+const server = app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
 
 // unhandled promise rejection
